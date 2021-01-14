02d89e96580f9761b53f5168b40eed5780ed6737
One Visage <onevisage@interia.pl>
2021-01-14 14:30:55 +0100

    achievements done

--- js/main.js
+++ js/main.js
@@ -21,7 +21,32 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 // slick JS
-    $('.your-class').slick({
+    $('.team-carousel').slick({
         // setting - name: setting - value
+        autoplay: true,
+        autoplaySpeed: 3500,
+        mobileFirst: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3
+                } 
+            },
+            {
+                breakpoint: 1600,
+                settings: {
+                    slidesToShow: 4
+                }
+            }
+        ]
     });
 });
