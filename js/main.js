52b3678767377522337f0386583fdad484c92ab4
One Visage <onevisage@interia.pl>
2021-01-14 08:46:18 +0100

    prices done

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-const nav = document.querySelector('.navbar');
-
+    const nav = document.querySelector('.navbar');
+// dodaje cień do nawigacji
     function addShadow() {
-    if(window.scrollY >= 300){
-        nav.classList.add('shadow-bg');
-    } else {
-        nav.classList.remove('shadow-bg');
-    }
+        if (window.scrollY >= 300) {
+            nav.classList.add('shadow-bg');
+        } else {
+            nav.classList.remove('shadow-bg');
+        }
     }
 
     window.addEventListener('scroll', addShadow);
 
-    $(document).ready(function () {
-        $(document).click(function (event) {
-            var clickover = $(event.target);
-            var _opened = $(".navbar-collapse").hasClass("show");
-            if (_opened === true && !clickover.hasClass("navbar-toggler")) {
-                $(".navbar-toggler").click();
-            }
-        });
+// skrypt naprawiający nawigację (zamyka się po kliknięciu w cokolwiek na mobilce)
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("show");
+        if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    });
+
+// slick JS
+    $('.your-class').slick({
+        // setting - name: setting - value
     });
 });
